cd621e3e838dd5cb5f331832d9ac018efd4ea02f
Braised-BBQ <166466797+Braised-BBQ@users.noreply.github.com>
2026-09-14 09:50:01 +0800

    log訊息修復

--- pet_backend/main.py
+++ pet_backend/main.py
@@ -606,4 +606,4 @@ async def websocket_endpoint(websocket: WebSocket):
 # 8. 啟動入口
 # -------------------------------------------------------------------
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
